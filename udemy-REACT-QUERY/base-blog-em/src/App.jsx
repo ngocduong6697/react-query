b0662067e29abdee1c54b501fd9180127a7a57c2
NGOC DUONG <apple@Ngocs-MacBook-Pro.local>
2021-12-07 10:26:39 +0700

    feat: react query dev tools

--- udemy-REACT-QUERY/base-blog-em/src/App.jsx
+++ udemy-REACT-QUERY/base-blog-em/src/App.jsx
@@ -1,5 +1,6 @@
 import { Posts } from "./Posts";
 import { QueryClient, QueryClientProvider} from 'react-query';
+import { ReactQueryDevtools } from "react-query/devtools"
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ function App() {
         <h1>Blog Posts</h1>
         <Posts />
       </div>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
