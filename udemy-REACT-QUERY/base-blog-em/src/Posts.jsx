8a8d78a319edcf963f416eb6a8c05e723df2ed6d
NGOC DUONG <apple@Ngocs-MacBook-Pro.local>
2021-12-07 15:01:03 +0700

    feat: pre-fetching the data

--- udemy-REACT-QUERY/base-blog-em/src/Posts.jsx
+++ udemy-REACT-QUERY/base-blog-em/src/Posts.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useQuery } from "react-query";
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
@@ -14,6 +14,16 @@ async function fetchPosts(pageNum) {
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(["posts", nextPage], () =>
+        fetchPosts(nextPage)
+      );
+    }
+  }, [currentPage, queryClient]);
 
   // replace with useQueryf
   const { data, isError, error, isLoading } = useQuery(
@@ -21,6 +31,7 @@ export function Posts() {
     () => fetchPosts(currentPage),
     {
       staleTime: 2000,
+      keepPreviousData: true,
     }
   );
   if (isLoading) return <h3>Loading...</h3>;
